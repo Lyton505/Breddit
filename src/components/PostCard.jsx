0f9091cf4fa12b94ad03c444ffc4388ec5d2cb2d
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-04-13 18:50:09 -0500

    added props

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,21 +3,22 @@ import "/src/styles/postCard.css";
 import upVoteArrow from "/src/assets/arrow-up.svg";
 import comments from "/src/assets/comments.svg";
 
-export default function PostCard() {
+export default function PostCard({
+                                   upvotes = 45,
+                                   title = "Bitcoin all new high",
+                                   author = "Satoshi Nakamoto",
+                                   creation_time = "10:56PM UTC",
+                                   imgUrl = bitCoinImg,
+                                   commentCount = 100
+                                 }) {
 
-  const upvotes = 45;
-  const title = "Bitcoin all new high";
-  const author = "Satoshi Nakamoto";
-  const creation_time = "10:56PM UTC";
-  const imgUrl = bitCoinImg;
-  const commentCount = 100;
 
   return (
     <div className={"postCard"}>
       <img src={imgUrl} alt="Bitcoin img" />
       <h4>{title}</h4>
       <p>Created at: {creation_time}</p>
-      <p >Created by: <span id={"author"}>{author}</span> </p>
+      <p>Created by: <span id={"author"}>{author}</span></p>
       <div className="postStats">
         <div>
           <img src={upVoteArrow} alt="up votes" id={"upVotesArr"} />
