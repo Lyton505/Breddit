6271518e7bdd9dde01d70eea793fcd66ef86bd75
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-04-13 23:47:51 -0500

    refactor: use browser router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import getRoutes from "./utils/routes.jsx";
+
+const routes = createBrowserRouter(getRoutes());
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes}></RouterProvider>
   </React.StrictMode>,
 );
