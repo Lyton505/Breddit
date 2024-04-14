b01895b2a25287cedf8639b6f4e6b5b4ecfec31f
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-04-13 23:47:02 -0500

    Use outletContext with supabase call

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
-import "./App.css";
+import "/src/styles/app.css";
+import Sidebar from "./components/SideBar.jsx";
+import { Outlet} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { supabase } from "./utils/client.js";
 
 function App() {
+
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    const getSupabasePosts = async () => {
+      const supabasePosts = await supabase.from("posts").select();
+      console.log(supabasePosts.data);
+      setPostData(supabasePosts.data);
+    };
+
+    getSupabasePosts();
+  }, []);
+
   return (
-    <>
-      <h1>Hey</h1>
-    </>
+    <div className={"appCont"}>
+      <Sidebar></Sidebar>
+      <div>
+        <Outlet context={[postData, setPostData]}></Outlet>
+      </div>
+    </div>
   );
 }
 
