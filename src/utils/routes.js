95fb769257fc580650f01aeba1cd7797e71649ae
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-04-13 19:06:40 -0500

    added routes

---
+++ src/utils/routes.js
@@ -0,0 +1,42 @@
+import App from "../App.jsx";
+import ErrorPage from "../pages/ErrorPage.jsx";
+import CreatePost from "../pages/CreatePost.jsx";
+import ViewPost from "../pages/ViewPost.jsx";
+import SignUp from "../pages/SignUp.jsx";
+import Home from "../pages/Home.jsx";
+import Login from "../pages/Login.jsx";
+
+export default function routes() {
+  return [
+    {
+      path: "/",
+      element: <App></App>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+          path: "/create",
+          element: <CreatePost></CreatePost>
+        },
+        {
+          path: "/post:id",
+          element: <ViewPost></ViewPost>
+        },
+        {
+          path: "/signup",
+          element: <SignUp></SignUp>
+        },
+        {
+          path: "/home",
+          element: <Home></Home>
+        },{
+          path: "/",
+          element: <Home></Home>
+        },
+        {
+          path: "/login",
+          element: <Login></Login>
+        }
+      ]
+    }
+  ];
+}
