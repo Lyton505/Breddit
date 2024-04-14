ba71b609e9221853e5e9ce897e03ac6218ff8321
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-04-14 14:58:18 -0500

    Feature: Login page with Github

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,33 @@
-export default function Login(){
+import React from 'react';
+import { supabase } from '../utils/client.js';
+import Button from '@mui/material/Button';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function LoginPage({signInWithGithub}) {
+
   return (
-    <div>
-      <h2>Login page</h2>
-    </div>
-  )
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <Typography variant="h" gutterBottom>
+        Sign in with GitHub
+      </Typography>
+      <Button
+        variant="contained"
+        startIcon={<GitHubIcon />}
+        onClick={signInWithGithub}
+        sx={{ mt: 2 }}
+      >
+        Login with GitHub
+      </Button>
+    </Box>
+  );
 }
