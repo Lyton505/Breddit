204883e78b0364293e880c66cbdad8786b898be7
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-04-14 15:41:47 -0500

    feature: default image before login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import "/src/styles/app.css";
-import Sidebar from "./components/SideBar.jsx";
-import { Outlet} from "react-router-dom";
 import { useEffect, useState } from "react";
 import { supabase } from "./utils/client.js";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./components/SideBar.jsx";
+import LoginPage from "./pages/Login.jsx";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  console.log("User state at start: ", user);
+
+  useEffect(() => {
+    checkUser();
+    window.addEventListener("hashchange", function() {
+      checkUser();
+    });
+  }, []);
+
+  async function checkUser() {
+    console.log("Checking user");
+    try {
+      const user = await supabase.auth.getUser();
+      setUser(user.data.user);
+    } catch (e) {
+      console.log("User not logged in");
+    }
+  }
+
+  async function signInWithGithub() {
+    const {
+      data,
+      error
+    } = await supabase.auth.signInWithOAuth({ provider: "github" });
+  }
+
+  async function signOut() {
+    await supabase.auth.signOut();
+    setUser(null);
+  }
+
 
   const [postData, setPostData] = useState([]);
 
@@ -19,12 +53,15 @@ function App() {
   }, []);
 
   return (
-    <div className={"appCont"}>
-      <Sidebar></Sidebar>
-      <div>
-        <Outlet context={[postData, setPostData]}></Outlet>
+    user === null ?
+      <LoginPage signInWithGithub={signInWithGithub}></LoginPage>
+      :
+      <div className={"appCont"}>
+        <Sidebar userInfo={user} signOut={signOut}></Sidebar>
+        <div>
+          <Outlet context={[postData, setPostData]}></Outlet>
+        </div>
       </div>
-    </div>
   );
 }
 
